7ac3ab47cb20dd72804aec3002cb5dc6b5aee153
srish-bhargava <srishti.bhargava1995@gmail.com>
2019-12-10 20:14:20 -0500

    Add files via upload

--- Bagofwords.py
+++ Bagofwords.py
@@ -1,18 +1,19 @@
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-
+from sklearn.feature_extraction import stop_words
+from nltk.stem import PorterStemmer
+import string
+ps = PorterStemmer()
 data = pd.read_csv("train.csv")
-count_vect = CountVectorizer(min_df=30)
-y_train_count = data['target'].to_numpy()
-X_train_count = count_vect.fit_transform(data['comment_text'])
-#X_test_count = count_vect.fit_transform(data_test['comment_text'])
-feature_array = X_train_count.toarray()
-loss = 0
-#Initialize the average square loss
-num_instances, num_features = feature_array.shape[0], feature_array.shape[1]
-theta = np.ones(num_features)
-num_instances = y_train_count.shape[0]
-loss = np.sum((np.dot(feature_array, theta) - y_train_count) ** 2) / num_instances
-grad = 2.0 / num_instances * np.dot((np.dot(feature_array, theta) - y_train_count), feature_array)
+for (index,comments) in enumerate(data['comment_text']):
+    comments = comments.translate(str.maketrans('','',string.punctuation))
+    print (comments)
+    l = comments.split()
+    data['comment_text'][index] = ' '.join([ps.stem(word) for word in l])
 
+print (data['comment_text'])
+count_vect = CountVectorizer(min_df=30, stop_words=stop_words.ENGLISH_STOP_WORDS)
+X_train_count = count_vect.fit_transform(data['comment_text'])
+feature_array = X_train_count.toarray()
+print (feature_array)
